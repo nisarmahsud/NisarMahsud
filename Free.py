a6cc04c349baf499e7aef4b2f1369bb64c3947c8
nisarmahsud <83748512+nisarmahsud@users.noreply.github.com>
2022-01-31 22:18:44 +0500

    Create Free.py

---
+++ Free.py
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nisarearning.com</title>
+    
+    <meta name="designer" href="https://uniquehyips.com">
+    
+    <link rel="icon" href="assets/img/favicon.png" type="image/gif" sizes="16x16">
+    <link rel="stylesheet" href="assets/css/base-style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+<body class="placeholder" data-scrollbar="#204163">
+    <div class="loader-wrapper ">
+        <div class="loader">
+            <span>L</span>
+            <span>O</span>
+            <span>A</span>
+            <span>D</span>
+            <span>I</span>
+            <span>N</span>
+            <span>G</span>
+        </div>
+    </div>
+    <!--menu call here-->
+    <div class="header-wrapper">
+            <!--header area start here-->
+    <header id="sticy_nav" class="header-area">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="header-menu header-shadow header-round d-flex justify-content-between align-items-center">
+                    <div id="rect" class="logo">
+                        <a href="/"><img src="assets/img/home2/logo.png" alt="logo here"></a>
+                    </div>
+                    <div class="pupko-menu">
+                        <nav class="main-navbar">
+                           <ul>
+                                <li>
+                                    <li><a href="/">HOME</a></li>
+                                        <li><a href="?a=about">ABOUT US</a></li>
+                                        <li><a href="?a=faq">FAQ</a></li>
+                                        <li><a href="?a=rules">RULES</a></li>
+                                        <li><a href="?a=support">CONTACT</a></li>
+                                    
+                                </li>
+                                                                <li class="btn-orange nav-btn"><a href="?a=login">Sign IN</a></li>
+                                <li class="btn-orange nav-btn"><a href="?a=signup">Sign UP</a></li>
+                                                            </ul>
+                        </nav>
+                    </div>
+                    <!-- responsive bar -->
+                    <div class="animate-bar">
+                        <div class="line1"></div>
+                        <div class="line2"></div>
+                        <div class="line3"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    </div>
+    <!--header area end here-->
+
+    <div class="cm-banner-area relative">
+        <div class="cm-banner-left">
+            <img class="circle" src="assets/img/home1/banner/banner-animation/circle.png" alt="">
+            <img class="small-circle" src="assets/img/home1/banner/banner-animation/small-circle.png" alt="">
+            <img class="cloud tf-c1" src="assets/img/home1/banner/banner-animation/cloud1.png" alt="">
+            <img class="small-cloud tf-m2" src="assets/img/home1/banner/banner-animation/cloud2.png" alt="">
+            <span class="round-md tf-r1"></span>
+            <span class="round-sm tf-r2"></span>
+            <span class="round-sm tf-r3"></span>
+            <span class="round-sm tf-r4"></span>
+            <span class="round-xs tf-r5"></span>
+         </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="cm-banner-content d-flex justify-content-between align-items-center get-bottom animate">
+                        <h2 class="get-left animate">About Us</h2>
+                        <nav aria-label="breadcrumb animate">
+                            <ol class="breadcrumb get-right animate">
+                                <li class="breadcrumb-item animate"><a href="/">Home</a></li>
+                                <li class="breadcrumb-item active animate">About Us</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="cm-banner-right">
+            <img class="circle" src="assets/img/home1/banner/banner-animation/circle.png" alt="">
+            <img class="small-circle" src="assets/img/home1/banner/banner-animation/small-circle.png" alt="">
+            <img class="cloud tf-c2" src="assets/img/home1/banner/banner-animation/cloud1.png" alt="">
+            <img class="small-cloud tf-m1" src="assets/img/home1/banner/banner-animation/cloud2.png" alt="" >
+            <span class="round-md tf-r1"></span>
+            <span class="round-sm tf-r2"></span>
+            <span class="round-sm tf-r3"></span>
+            <span class="round-sm tf-r4"></span>
+            <span class="round-xs tf-r5"></span>
+         </div>
+    </div>
+    <!--h2-we-are area start here-->
+     <!--uk certificate area start here-->
+    <section class="we-are-area section-bg-padding">
+        <div class="we-are-img">
+            <div class="we-are-banner-img">
+                <img src="assets/img/uk.png" alt="">
+            </div>
+            <div class="we-are-animation">
+                <img src="assets/img/home2/we-are/like.png" alt="" class="h2-like">
+                <img src="assets/img/home2/we-are/heart.png" alt="" class="h2-heart">
+                <img src="assets/img/home2/we-are/message.png" alt="" class="h2-message">
+                <img src="assets/img/home2/we-are/square.png" alt="" class="h2-square">
+                <img src="assets/img/home2/we-are/circle.png" alt="" class="moving-circle">
+                <img src="assets/img/home2/we-are/circle2.png" alt="" class="moving-circle-right">
+            </div>
+        </div>
+        <div class="we-are-img">
+            <div class="we-are-banner-img">
+                <img src="assets/img/about.png" alt="">
+            </div>
+        </div>
+        <div class="container">
+            <div class="row justify-content-end">
+                <div class="col-lg-6">
+                    <div class="we-are-content get-bottom animate">
+                        <div class="section-title animate">
+                            <h2 class="pb-20">>Registered Company under FBR| Federal Board of Revenue - Government of Pakistan</h2>
+                        </div>
+                        <p class="pb-50 animate">Totally name his such his had state biases, perceive we it duckthemed negotiations least, at to completely two of self-interest, right rome; Little acknowledge slightly said titled proposal one negotiations back client thought, following caching alone,</p>
+                        <div class="animate">
+                            <a href="assets/img/d.pdf">Download PDF</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--uk certificate area end here-->
+    
+    <!--Features start here-->
+    <section class="h2-we-make section-bg-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="h2-we-make-heading pb-50 get-bottom animate">
+                        <div class="section-title animate">
+                            <h2 class="pb-20">The Features of our company</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 mb-50 mb-50 mb-sm-25">
+                    <div class="h2-we-make-card relative ml-50 br-6 card-yellow get-bottom animate">
+                        <div class="h2-we-make-icone d-center icon-shadow icon-yellow-bg">
+                            <img src="assets/img/svg/optimization.svg" alt="">
+                        </div>
+                        <div class="h2-we-make-content get-bottom animate">
+                            <div class="card-title">
+                                <h3 class="pb-35 pb-md-20 animate">Website Security</h3>
+                            </div>
+                            <p class="animate">Your brand is the core of your marketing, the central theme around your products and services.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mb-50 mb-50 mb-sm-25">
+                    <div class="h2-we-make-card relative ml-50 br-6 card-paste get-bottom animate">
+                        <div class="h2-we-make-icone d-center icon-shadow icon-paste-bg">
+                            <img src="assets/img/svg/framework.svg" alt="">
+                        </div>
+                        <div class="h2-we-make-content get-bottom animate">
+                            <div class="card-title">
+                                <h3 class="pb-35 pb-md-20 animate">Referral System</h3>
+                            </div>
+                            <p class="animate">Your brand is the core of your marketing, the central theme around your products and services.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mb-md-50 mb-sm-25">
+                    <div class="h2-we-make-card relative ml-50 br-6 card-purple get-bottom animate">
+                        <div class="h2-we-make-icone d-center icon-shadow icon-purple-bg">
+                            <img src="assets/img/svg/analysis.svg" alt="">
+                        </div>
+                        <div class="h2-we-make-content get-bottom animate">
+                            <div class="card-title">
+                                <h3 class="pb-35 pb-md-20 animate">LiveChat Support</h3>
+                            </div>
+                            <p class="animate">Your brand is the core of your marketing, the central theme around your products and services.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="h2-we-make-card relative ml-50 br-6 card-orange get-bottom animate">
+                        <div class="h2-we-make-icone d-center icon-shadow icon-orange-bg">
+                            <img src="assets/img/svg/infographic.svg" alt="">
+                        </div>
+                        <div class="h2-we-make-content get-bottom animate">
+                            <div class="card-title">
+                                <h3 class="pb-35 pb-md-20 animate">Instant Withdraw</h3>
+                            </div>
+                            <p class="animate">Your brand is the core of your marketing, the central theme around your products and services.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Features end here-->   
+    
+  
+  
+    <!--footer area start here-->
+    <footer class="footer-area relative pt-100 pt-md-60 bg-blue2">
+        <div class="footer-animation">
+            <img class="footer-ani1" src="assets/img/home2/footer-ani.png" alt="">
+            <img class="footer-ani2" src="assets/img/home2/footer-ani.png" alt="">
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="footer-heading d-flex justify-content-between align-items-center pb-50">
+                        <div class="section-title get-left animate">
+                            <h2 class="text-white">Interested To<br>Get Our Featured Service</h2>
+                        </div>
+                        <div class="get-right animate">
+                            <a href="?a=signup" class="btn btn-sm btn-radius bg-white btn-shatter-ired btn-animate">Get Started Now</a>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="copy-right h2-cr text-center py-50 d-flex justify-content-between align-items-center animate get-top">
+                        <img class="mx-width-150" src="assets/img/home2/logo.png" alt="logo here">
+                        <p class="text-gray">©fbrearning.com 2021. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--footer area end here-->
+    <!--footer script call here-->
+
+    <!-- DEFAULT JQUERY SCRIPT HERE
+        ================================================== -->
+    <script src="assets/js/default/jquery-3.3.1.min.js"></script>
+    <script src="assets/js/default/popper.js"></script>
+    <script src="assets/js/default/bootstrap.min.js"></script>
+    <script src="assets/js/default/jquery-ui.min.js"></script>
+    <script src="assets/js/default/jquery-migrate-1.4.1.min.js"></script>
+
+    <!-- ALL ANIMATION SCRIPT CALL HERE 
+    ====================================-->
+    <!-- animation plugin script -->
+    <script src="assets/js/plugin/animation-js/gsap.min.js"></script>
+    <script src="assets/js/plugin/animation-js/scroll-tigger.js"></script>
+    <script src="assets/js/plugin/animation-js/scroll-magic.js"></script>
+    <script src="assets/js/plugin/animation-js/nicescroll.min.js"></script>
+    <script src="assets/js/plugin/animation-js/aos.js"></script>
+
+    <!-- ALL PLUGIN SCRIPT CALL HERE 
+    ====================================-->
+    <!--skick script-->
+    <script src="assets/js/plugin/slick.min.js"></script>
+
+    <!--Owl script-->
+    <script src="assets/js/plugin/owl.carousel.min.js"></script>
+    <script src="assets/js/plugin/owl-custom.js"></script>
+
+    <!--Waypoint script-->
+    <script src="assets/js/plugin/jquery.waypoints.min.js"></script>
+
+    <!--Counterup script-->
+    <script src="assets/js/plugin/jquery.counterup.min.js"></script>
+
+    <!--Youtube overlay script-->
+    <script src="assets/js/plugin/youtube-overlay.js"></script>
+
+    <!--swiper script-->
+    <script src="assets/js/plugin/swiper.min.js"></script>
+
+    <!--Parallax script-->
+    <script src="assets/js/plugin/simpleParallax.min.js"></script>
+
+    <!-- Nice Select -->
+    <script src="assets/js/plugin/nice-select.min.js"></script>
+    <!-- Elevatezoom  script-->
+    <script src="assets/js/plugin/jquery.elevatezoom.js"></script>
+
+
+    <!--isotope script-->
+    <script src="assets/js/plugin/isotop-pakage-min.js"></script>
+    <script src="assets/js/plugin/image-loaded.min.js"></script>
+
+    <!-- CUSTOM SCRIPT CALL HERE 
+    ====================================-->
+    <script src="assets/js/tl-custom.js"></script>
+    <script src="assets/js/mouse-event.js"></script>
+    <script src="assets/js/scrollTrigger.js"></script>
+    <script src="assets/js/scripts.js"></script>
+
+
+</body>
+
+</html>
